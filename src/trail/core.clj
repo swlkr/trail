f677776c759ad7891ba06e461e758f3d478d54b7
Sean Walker <swlkr.rbl@gmail.com>
2017-08-16 09:25:31 -0700

    Add route matching/mapping

--- src/trail/core.clj
+++ src/trail/core.clj
@@ -1,6 +1,47 @@
 (ns trail.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def routes (atom {}))
+
+(defn route [{:keys [uri method fn]}]
+  (when
+    (and (every? #(not (nil? %)) [uri method fn])
+         (contains? #{:get :put :post :delete} method)
+      (let [segments (filter #(not (empty? %)) (clojure.string/split uri #"/"))]
+        (swap! routes update-in (list segments) assoc method fn)))))
+
+(defn seg-matches? [route-seg uri-seg]
+  (and (= (first route-seg) (first uri-seg))
+       (= (count route-seg) (count uri-seg))))
+
+(defn seg? [str]
+  (not (empty? str)))
+
+(defn segments [uri]
+  (when uri
+    (filter seg? (clojure.string/split uri #"/"))))
+
+(defn route-match-map [route-map uri]
+    (let [segs (segments uri)
+          routes (filter #(seg-matches? segs %) (keys route-map))]
+      (first (map #(zipmap % segs) routes))))
+
+(defn route-params [match-map]
+  (let [ks (keys match-map)
+        param-keys (filter #(clojure.string/starts-with? % ":") ks)
+        param-str-map (select-keys match-map param-keys)]
+    (into {} (map (fn [[k v]] [(keyword (subs k 1)) v]) param-str-map))))
+
+(defn get-fn [request-method route-map match-map]
+  (let [k (keys match-map)
+        fns (get route-map k)]
+    (get fns request-method)))
+
+(defn match-route [{:keys [request-method uri]}]
+  (when (every? #(not (nil? %)) [request-method uri])
+    (let [route-map @routes
+          match-map (route-match-map route-map uri)
+          params (route-params match-map)
+          f (get-fn request-method route-map match-map)]
+      (if (nil? f)
+        nil
+        {:fn f :params params}))))
--- test/trail/core_test.clj
+++ test/trail/core_test.clj
@@ -1,7 +1,37 @@
 (ns trail.core-test
   (:require [clojure.test :refer :all]
-            [trail.core :refer :all]))
+            [trail.core :as trail]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest route-test
+  (testing "adding a route"
+    (let [_ (trail/route {:method :get
+                          :uri "/users"
+                          :fn :all})]
+      (is (= {'("users") {:get :all}} (deref trail/routes)))))
+
+  (testing "adding an invalid route"
+    (let [_ (reset! trail/routes {})
+          _ (trail/route {:method nil :uri "/users" :fn :all})]
+      (is (= {} (deref trail/routes)))))
+
+  (testing "just adding nil"
+    (let [_ (reset! trail/routes {})
+          _ (trail/route nil)]
+      (is (= {} (deref trail/routes))))))
+
+(deftest match-route-test
+  (testing "matching nil"
+    (is (= nil (trail/match-route nil))))
+
+  (testing "matching nil vals"
+    (is (= nil (trail/match-route {:uri nil :method nil}))))
+
+  (testing "matching route that doesn't exist"
+    (is (= nil (trail/match-route {:uri "/users" :method :get}))))
+
+  (testing "matching route that does exist"
+    (let [_ (trail/route {:method :get
+                          :uri "/users"
+                          :fn :all})]
+      (is (= {:fn :all :params {}} (trail/match-route {:uri "/users"
+                                                       :request-method :get}))))))
