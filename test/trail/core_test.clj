331910a8b04ebf5f9f485dfb8759e690a46dda0e
Sean Walker <swlkr.rbl@gmail.com>
2017-08-20 15:40:55 -0700

    Simplify tests

--- test/trail/core_test.clj
+++ test/trail/core_test.clj
@@ -3,41 +3,39 @@
             [trail.core :as trail]))
 
 (deftest route-test
-  (testing "adding a route"
-    (let [_ (trail/route {:method :get
-                          :uri "/users"
-                          :fn :all})]
-      (is (= {'("users") {:get :all}} (deref trail/routes)))))
+  (let [_ (trail/route {:method :get :uri "/users" :fn :all})
+        _ (trail/route {:method :post :uri "/users" :fn :create})
+        _ (trail/route {:method :get :uri "/" :fn :home})]
+    (testing "adding a route"
+        (is (= {'("/users") {:get :all :post :create}
+                '("/") {:get :home}}
+               (deref trail/routes))))
 
-  (testing "adding an invalid route"
-    (let [_ (reset! trail/routes {})
-          _ (trail/route {:method nil :uri "/users" :fn :all})]
-      (is (= {} (deref trail/routes)))))
+    (testing "adding an invalid route"
+      (let [_ (trail/route {:method nil :uri "/users" :fn :all})]
+        (is (= {'("/users") {:get :all :post :create}
+                '("/") {:get :home}}
+               (deref trail/routes)))))
 
-  (testing "just adding nil"
-    (let [_ (reset! trail/routes {})
-          _ (trail/route nil)]
-      (is (= {} (deref trail/routes))))))
+    (testing "just adding nil"
+      (let [_ (trail/route nil)]
+        (is (= {'("/users") {:get :all :post :create}
+                '("/") {:get :home}}
+               (deref trail/routes))))))
 
-(deftest match-route-test
-  (testing "matching nil"
-    (is (= nil (trail/match-route nil))))
+  (deftest match-route-test
+    (testing "matching nil"
+      (is (= nil (trail/match-route nil))))
 
-  (testing "matching nil vals"
-    (is (= nil (trail/match-route {:uri nil :method nil}))))
+    (testing "matching nil vals"
+      (is (= nil (trail/match-route {:uri nil :method nil}))))
 
-  (testing "matching route that doesn't exist"
-    (is (= nil (trail/match-route {:uri "/users" :method :get}))))
+    (testing "matching route that doesn't exist"
+      (is (= nil (trail/match-route {:uri "/users" :method :get}))))
 
-  (testing "matching route that does exist"
-    (let [_ (trail/route {:method :get
-                          :uri "/users"
-                          :fn :all})]
+    (testing "matching route that does exist"
       (is (= {:fn :all :params {}} (trail/match-route {:uri "/users"
-                                                       :request-method :get})))))
+                                                       :request-method :get}))))
 
-  (testing "match /"
-    (let [_ (trail/route {:method :get
-                          :uri "/"
-                          :fn :home})])
-    (is (= {:fn :home :params {}} (trail/match-route {:uri "/" :request-method :get})))))
+    (testing "match /"
+      (is (= {:fn :home :params {}} (trail/match-route {:uri "/" :request-method :get}))))))
